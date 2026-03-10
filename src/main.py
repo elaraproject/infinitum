2dd2255c3489aff78808d0167f365e015a1efa2f
NHWX-git <jstevethom@gmail.com>
2026-03-10 16:42:56 -0400

    Re-organized code
    Organized code that was previously out in the open that could be re-organized into functions into functions. Two functions were created from this: process_raw_text() which process the text such that it can be easily read by the LaTeX function without error, and then the solve_differential_equation() which actually processes the text and returns the solutions to the differential equation.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,30 @@ from sympy.parsing.latex import parse_latex
 from elara_symbolic.calculate import *
 from st_mathlive import mathfield
 
+#function for processing the mathlive user input into text that can be process by sympy
+def process_raw_text(Tex: str):
+    #NOTE: BUG EXISTS WHERE IT MAY HAVE TROUBLE PARSING CERTAIN UPRIGHT EQUATIONS, DO NOT CHECK THAT BOX
+    newTex = Tex.replace(r"\differentialD", "d")
+    #parse for upright equations TODO: Remove the option for upright equations
+    if (newTex[0:8] == "\mathrm{"):
+        newTex = newTex[8:-1]
+
+    return newTex
+
+def solve_differential_equation(Tex: str):
+    #parse the LaTeX provided by the user into a differential equation
+    expr = parse_latex(Tex, strict=False)
+    # define your unknown function
+    # to be solved for
+    # here it is y(x)
+    y = Function("y")(x)
+    # define a dummy constant for solving the differential equation
+    k = Symbol("k", constant=True, real=True)
+    #solve the differential equation itself
+    de_sols = solve_ode(expr, y, y0=0.0, t_span=(lowerRange, upperRange), constants=[(k, 1.0)], step_size=0.01)
+
+    return de_sols
+
 st.write("""
 # Elara-symbolic UI
 
@@ -12,19 +36,10 @@ Currently being developed...
 
 Tex, MathML = mathfield(title="Enter Equations Here", value=r"\frac{dy}{dx} = y(1 - y)", mathml_preview=True, upright=False)
 
-#NOTE: BUG EXISTS WHERE IT MAY HAVE TROUBLE PARSING CERTAIN UPRIGHT EQUATIONS, DO NOT CHECK THAT BOX
-Tex = Tex.replace(r"\differentialD", "d")
-#parse for upright equations TODO: Remove the option for upright equations
-if (Tex[0:8] == "\mathrm{"):
-    Tex = Tex[8:-1]
-print(Tex)
-expr = parse_latex(Tex, strict=False)
+#Code for preliminary processing of the LaTeX
+Tex = process_raw_text(Tex)
 
-# define your unknown function
-# to be solved for
-# here it is y(x)
-y = Function("y")(x)
-
+#This is the code for the components letting the user set the bounds of the graph
 lowerRange = st.number_input(label="Enter Lower Number Bound: ", value=0.0)
 upperRange = st.number_input(label="Enter Upper Number Bound: ", value=1.0)
 
@@ -32,10 +47,7 @@ upperRange = st.number_input(label="Enter Upper Number Bound: ", value=1.0)
 if upperRange <= lowerRange:
     st.write("Unable to display equation: lower bound is greater than or equal to lower bound ")
 else:
-    # define a dummy constant for solving the differential equation
-    k = Symbol("k", constant=True, real=True)
-    #solve the differential equation itself
-    de_sols = solve_ode(expr, y, y0=0.0, t_span=(lowerRange, upperRange), constants=[(k, 1.0)], step_size=0.01)
+    de_sols = solve_differential_equation(Tex)
     if type(de_sols) != type(None):
         st.line_chart(de_sols['y'])
     else:
