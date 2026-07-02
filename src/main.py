7bd95cd69b12908fd6e6ff972d5da9ffe1b1f8bf
NHWX-git <jstevethom@gmail.com>
2026-07-02 01:02:37 -0400

    Integrated a Function for Converting 3rd Order Differential Equations into a Matrix and Made Some bug fixes
    I fixed a few small errors with parsing higher order differnetial equations primarily one where it would sometimes fail to parse in certain situations where there were multiple higher order differentials and also condensed the code for fixing notation parsed poorly by sympy with regards to these higher order differentials. Furthermore I added a system for getting a matrix from a given 3rd order differential equation. In the future I hope to generalize this to n-order differential equations and have already partially designed these functions as such though not completely yet obviously.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,10 @@
 import streamlit as st
 from sympy.parsing.latex import parse_latex
-from sympy import Derivative, Symbol, Function, Mul, symbols
+from sympy import Derivative, Symbol, Function, Mul
 from sympy.core.function import AppliedUndef # Crucial for finding mistaken function calls
 from elara_symbolic.cas import solve_ode
 from st_mathlive import mathfield
 import polars as pl
-from PIL import Image
-import re
-
-#Load in the image from our library
-icon = Image.open(r'src/favicon-144x144.png')
-#This gives the page a title and icon in the browser so it is more identifiable
-st.set_page_config(page_title="Infinitum", page_icon=icon)
 
 if "app_loaded" not in st.session_state:
     st.toast("App loading...", icon="ℹ", duration=2)
@@ -19,7 +12,6 @@ if "app_loaded" not in st.session_state:
 
 # function for processing the mathlive user input into text that can be process by sympy
 def process_raw_text(Tex: str):
-    print(f"RAW TEX: {Tex}")
     # we need this here because the mathfield often processes a simple d as this differentialD
     # which confuses sympy so we replace that with the simple d.
     newTex = Tex.replace(r"\differentialD", "d")
@@ -46,63 +38,22 @@ def fix_implicit_multiplication(node):
             return Symbol(func_name) * Mul(*args)
     return node
 
-def solve_differential_equation(upperRange: int, lowerRange: int, stepSize: int, Tex: str, constantValues: dict, Y0: float, leapfrogVal: bool):
+def solve_differential_equation(upperRange: int, lowerRange: int, stepSize: int, Tex: str, constantValues: dict, Y0: float, leapfrog: bool):
     # parse the LaTeX provided by the user into a differential equation
     expr = parse_latex(Tex, strict=False)
-    secondOrder = False
-
-    def prepare_higher_order_diffeq(eq, dependent_symb, func_symbol):
-        func_symbols = f"{func_symbol}_æ"
-        symbol_string = f"{func_symbol} _ æ"
-        tup = symbols(symbol_string, cls=Function)
-        for i in range(0,len(tup)-1):
-            substitute = Function(func_symbols[i])(dependent_symb)
-            conv = Function(func_symbols[i+1])(dependent_symb)
-            print(f"EQ: {eq}, {substitute}, {conv}\n")
-            eq = eq.subs(substitute.diff(dependent_symb), conv)
-        return eq
-
-    def fixHigherOrderDiffeqs(layer):
-        nonlocal expr
-        nonlocal secondOrder
-        curLayer = str(layer) # We make it a string so its easy to parse, the fragment of the equation we are checking
-        #First exit case is if we reach the end and haven't found the target expression of a higher
-        #order diffeq we exit
-        if curLayer == "": 
-            return False
-        #if we have found the target equation, then we go ahead and substitute it in
-        if re.fullmatch(r"\(d\*\*\d\*[a-z]\)\/\(d[a-z]\*\*\d\)", curLayer):
-            #Get the function, degree, and dependent variable
-            num = curLayer[4]
-            denomFuncChar = curLayer[6]
-            numFuncChar = curLayer[11]
-
-            expr = expr.subs(layer, Derivative(Function(denomFuncChar)(Symbol(numFuncChar)),Symbol(numFuncChar),int(num)))
-            secondOrder = True
-            #let our loop know it should exit
-            return True
-        for i in layer.args:
-            if fixHigherOrderDiffeqs(i):
-                return True
-        return False
-
+    
+    
     # create a dictionary of constants for the parsing
     parseConstants = { i : Symbol(i, constant=True, real=True) for i in constantValues.keys() }
 
-    #Gotta do this before we find derivatives to find all the improperly parsed derivatives and convert them to properly parsed
-    fixHigherOrderDiffeqs(expr)
     # Find all derivatives to figure out what the dependent function is
     derivatives = expr.atoms(Derivative)
     
     # get the functions that are not straight 
     dep_funcs = {deriv.args[0] for deriv in derivatives}
- 
+    
     dep_node = list(dep_funcs)[0]
     deriv = list(derivatives)[0]
-    #if secondOrder:
-    #    expr = prepare_higher_order_diffeq(expr, str(dep_node)[2], str(dep_node)[0])
-    print(f"EXPR: {expr}")
-
     
     # SymPy sometimes parses the derivative dependent var as a Symbol ('y'), sometimes as a Function ('y(x)')
     if isinstance(dep_node, AppliedUndef):
@@ -134,20 +85,16 @@ def solve_differential_equation(upperRange: int, lowerRange: int, stepSize: int,
     
     # solve the differential equation itself
     #This implements one algorithm for solving differential equations
-    if not leapfrogVal:
-        de_sols = solve_ode(expr, dep_func, y0=Y0, v0=0, t_span=(lowerRange, upperRange), constants=constantPass, step_size=stepSize)
-    #if there is only one arg we should not use the leapfrog algorithm
-    elif len(dep_funcs) <= 1: 
-        de_sols = solve_ode(expr, dep_func, y0=Y0, v0=0, t_span=(lowerRange, upperRange), constants=constantPass, step_size=stepSize)
-    #This implements a different algorithm, the leapfrog algorithm for higher order differential equations
-    #elif leapfrogVal:
-    #if True:
-    #    de_sols = leapfrog(expr, x0=Y0, v0=None, t_span=(lowerRange, upperRange), fun_args=constantPass, step_size=stepSize, progress_bar=False, show_time_exec=False)
+    if not leapfrog:
+        de_sols = solve_ode(expr, dep_func, y0=Y0, t_span=(lowerRange, upperRange), constants=constantPass, step_size=stepSize)
+    #This implements a different algorithm, the leapfrog algorithm
+    elif leapfrog:
+        leapfrog(expr, y0=Y0, v0=None, t_span=(lowerRange, upperRange), fun_args=constantPass, step_size=stepSize, progress_bar=False, show_time_exec=False)
 
     return de_sols
 
 # takes the equation, and the bounds and produces a graph from it
-def process_input_and_graph(upperRange: int, lowerRange: int, stepSize: int, Tex: str, constantValues: dict, Y0: float, leapfrog: bool):
+def process_input_and_graph(upperRange: int, lowerRange: int, stepSize: int, Tex: str, constantValues: dict, Y0: float):
     if upperRange <= lowerRange:
         st.write("Unable to display equation: lower bound is greater than or equal to upper bound.")
     else:
@@ -163,7 +110,7 @@ def process_input_and_graph(upperRange: int, lowerRange: int, stepSize: int, Tex
             # while the differential equation is solved
             while not solve_complete:
                 try:
-                    de_sols = solve_differential_equation(upperRange, lowerRange, stepSize, Tex, constantValues, Y0, leapfrog)
+                    de_sols = solve_differential_equation(upperRange, lowerRange, stepSize, Tex, constantValues, Y0)
                 except ValueError as e:
                     st.error(f"Solve unsuccessful: {str(e)}")
                 solve_complete = True
@@ -228,11 +175,10 @@ for letter in selected_constants:
     constant_values[letter] = st.number_input(label=f"enter constant value for {letter}: ", value=0.0)
 
 # This is the code for the components letting the user set the bounds of the graph
-<<<<<<< HEAD
 lowerRange = st.number_input(label="Enter Lower Number Bound (initial x): ", value=0.0)
 upperRange = st.number_input(label="Enter Upper Number Bound (final x): ", value=5.0)
 stepSize = st.number_input(label="Enter Step Interval: ", format="%.3f", value=0.001, step=1E-3)
 Y0 = st.number_input(label="Enter Y0 of the Differential Equation: ", format="%.2f", value=0.50) # Set the initial condition
-selected_constants = st.selectbox(label="Enter The Function You Wish To Use Here:", options=["Leapfrog", "Base"])
+selected_constants = st.selectbox(label="Enter The Solving Method You Wish To Use Here:", options=["Base", "Leapfrog"])
 
 st.button(label="Solve Differential Equation", on_click=lambda: process_input_and_graph(upperRange, lowerRange, stepSize, Tex, constant_values, Y0))
